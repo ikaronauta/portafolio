4de1d8b645e352bf1938d6f45e562a0d81ea7537
ikaronauta <jaortiz0496@misena.edu.co>
2022-01-11 17:12:58 -0500

    validacion login

--- administrador/index.php
+++ administrador/index.php
@@ -5,12 +5,12 @@
         <form action="#" method="post">
             <div class="form-group">
                 <label for="email">Correo Electronico</label><br>
-                <input type="email" name="email" id="email">
+                <input type="email" name="email" id="email" autofocus required>
             </div>
 
             <div class="form-group">
                 <label for="password">Contraseña</label><br>
-                <input type="password" name="password" id="password">            
+                <input type="password" name="password" id="password" required>            
             </div>
 
             <div class="form-group">
