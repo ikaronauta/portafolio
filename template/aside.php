8027725e0c8876098fe440f55f9d62b386e1efa8
ikaronauta <jaortiz0496@misena.edu.co>
2022-01-11 15:21:17 -0500

    update estilos aside

--- template/aside.php
+++ template/aside.php
@@ -2,6 +2,8 @@
     <div class="title">
         Destacados
     </div>
+
+    <div class="cajon-widgets">
     <div class="widget">
         <a href="#">
             <h3>Franchesca Accesorios</h3>
@@ -9,5 +11,13 @@
             <img src="img/franchesca.jpeg" alt="franchesca">
         </a>
     </div>
-    
+    <div class="widget">
+        <a href="#">
+            <h3>Franchesca Accesorios</h3>
+            <p>Tienda en línea especializada en accesorios para mujer.</p>
+            <img src="img/franchesca.jpeg" alt="franchesca">
+        </a>
+    </div>
+
+    </div>    
 </aside>
