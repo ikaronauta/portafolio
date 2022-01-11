7993b6a032dd8e6b2dde9406ee2825c71211bc18
ikaronauta <jaortiz0496@misena.edu.co>
2022-01-11 16:32:41 -0500

    add css/login.css

---
+++ administrador/index.php
@@ -0,0 +1,7 @@
+<?php include '../administrador/template/header.php'; ?>
+
+<div class="login-form">
+    <p>hola mundo</p> hola mundo
+</div>
+
+<?php include '../administrador/template/footer.php'; ?>
