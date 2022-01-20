72ef7012194862d9e28531b195838e5dd1b95bfd
ikaronauta <jaortiz0496@misena.edu.co>
2022-01-19 21:28:59 -0500

    update estilos img sobre mí

--- view/sobremi.php
+++ view/sobremi.php
@@ -2,7 +2,7 @@
     <article>
         <h1>Sobre mí</h1><hr><br>
         <div class="articulo-sobremi">
-            <img src="img/hijos.jpg" alt="Sobre mí" class="img-blog-left" width="290" height="210px">
+            <!--<img src="img/hijos.jpg" alt="Sobre mí" class="img-blog-left" width="290" height="210px">-->
             <div class="articulo1-sobremi">
                 <p>Originario de la ciudad de Manizales, tengo 38 años y soy padre de <b><em>María del Mar</em></b> y <b><em>Miguel Ángel</em></b>, quienes son mi inspiración.</p>
                 <p>Me encantan los gatos y me gusta montar en bicicleta.</p>
